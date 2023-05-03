d7a9d950f9f1d72cee1b9a90430691e1b51c73ec
Charlotte Bartholomé <charlotte.bartholome@gmail.com>
2023-05-03 17:45:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Pokedex';
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(event: MouseEvent){
+    const index: number = +(event.target as HTMLInputElement).value;
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  }
+
 }
